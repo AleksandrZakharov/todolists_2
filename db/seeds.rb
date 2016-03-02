afd9e7783e24a909722457dcf87fa489a46d972b
Aleksandr Zakharov <human.of.rain@gmail.com>
2016-03-02 21:40:14 +0300

    rq09.3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ User.create! [
 	{username: "Clinton", password_digest: "password1"},
 ]
 
-User.find_by!(username: "Fiorina").create_profile(gender: "F", birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
-User.find_by!(username: "Trump").create_profile(gender: "M", birth_year: 1946, first_name: "Donald", last_name: "Trump")
-User.find_by!(username: "Carson").create_profile(gender: "M", birth_year: 1951, first_name: "Ben", last_name: "Carson")
-User.find_by!(username: "Clinton").create_profile(gender: "F", birth_year: 1947, first_name: "Hillary", last_name: "Clinton")
+User.find_by!(username: "Fiorina").create_profile(gender: "female", birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
+User.find_by!(username: "Trump").create_profile(gender: "male", birth_year: 1946, first_name: "Donald", last_name: "Trump")
+User.find_by!(username: "Carson").create_profile(gender: "male", birth_year: 1951, first_name: "Ben", last_name: "Carson")
+User.find_by!(username: "Clinton").create_profile(gender: "female", birth_year: 1947, first_name: "Hillary", last_name: "Clinton")
 
 
 
-User.all.each { |u| u.todo_lists.create(list_name: "#{u.username}", list_due_date: "#{Date.today}")}
+User.all.each { |u| u.todo_lists.create(list_name: "#{u.username}", list_due_date: Date.today + 1.year)}
