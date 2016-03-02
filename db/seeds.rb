76c6c792d57e70b5b11dc186f112d9956a11cea3
human.of.rain <hauman.of.rain@gmail.com>
2016-03-02 14:01:48 +0300

    r09

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+User.create! [
+	{username: "Fiorina", password_digest: "abc123"},
+	{username: "Trump", password_digest: "123abc"},
+	{username: "Carson", password_digest: "not_telling"},
+	{username: "Clinton", password_digest: "password1"},
+]
+
+User.find_by!(username: "Fiorina").create_profile(gender: "F", birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
+User.find_by!(username: "Trump").create_profile(gender: "M", birth_year: 1946, first_name: "Donald", last_name: "Trump")
+User.find_by!(username: "Carson").create_profile(gender: "M", birth_year: 1951, first_name: "Ben", last_name: "Carson")
+User.find_by!(username: "Clinton").create_profile(gender: "F", birth_year: 1947, first_name: "Hillary", last_name: "Clinton")
+
+User.find_by!(username: "Fiorina")
+User.find_by!(username: "Trump")
+User.find_by!(username: "Carson")
+User.find_by!(username: "Clinton")
